7a2fd996d5b07e97ef09a22d4b12b0cc7352b5bf
Anatol <64561080+HimikMain@users.noreply.github.com>
2021-11-30 23:30:11 +0300

    3-ая лаба

---
+++ SPPLab3/SPPLab3/Mutex.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+
+namespace SPPLab3
+{
+    public class Mutex
+    {
+        private Thread lockingThread;
+
+        public void Lock()
+        {
+            while (Interlocked.CompareExchange(ref lockingThread, Thread.CurrentThread, null) != null) {}
+        }
+        public void Unlock()
+        {
+            Interlocked.Exchange(ref lockingThread, null);
+        }
+    }
+}
---
+++ SPPLab3/SPPLab3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace SPPLab3
+{
+    class Program
+    {
+        static Mutex mutex = new Mutex();
+
+        public static void Main()
+        {
+            Thread fThread = new Thread(Paint);
+            Thread sThread = new Thread(Paint);
+
+            fThread.Start();
+            sThread.Start();
+
+        }
+
+        static void Paint()
+        { 
+            mutex.Lock();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write("#");
+                    Thread.Sleep(20);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            mutex.Unlock();
+        }
+    }
+}
